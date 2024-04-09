8741735c719e18cca6c473311033a793db53f610
Raúl Martín <raul.martin@urjc.es>
2024-04-09 19:30:20 +0200

    mlc add from getenv()

--- CTFd/utils/config/integrations.py
+++ CTFd/utils/config/integrations.py
@@ -1,12 +1,12 @@
 from CTFd.utils import get_app_config, get_config
-
+from os import getenv
 
 def mlc():
     admin_config = get_config("oauth_client_id") and get_config("oauth_client_secret")
-    main_config = get_app_config("OAUTH_CLIENT_ID") and get_app_config(
-        "OAUTH_CLIENT_SECRET"
-    )
-    return admin_config or main_config
+    main_config = get_app_config("OAUTH_CLIENT_ID") and get_app_config("OAUTH_CLIENT_SECRET")
+    openid_config = getenv("OAUTH_CLIENT_ID") and getenv("OAUTH_CLIENT_SECRET")
+
+    return admin_config or main_config or openid_config
 
 
 def mlc_registration():
